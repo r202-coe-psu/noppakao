5aeac00a249af8bd1e7e73429e00d6165877f230
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2024-07-18 02:35:40 +0700

    ✨ feat:edit event

--- noppakao/web/views/admin/events.py
+++ noppakao/web/views/admin/events.py
@@ -27,23 +27,27 @@ def index():
 
 
 @module.route("/create", methods=["GET", "POST"], defaults={"event_id": None})
-@module.route("<event_id>/edit")
+@module.route("<event_id>/edit", methods=["GET", "POST"])
 @acl.roles_required("admin")
 def create_or_edit(event_id):
     form = forms.events.EventForm()
+    event = models.Event()
+
+    if event_id:
+        event = models.events.Event.objects(id=event_id).first()
+        form = forms.events.EventForm(obj=event)
 
     if not form.validate_on_submit():
         print(form.errors)
         return render_template("/admin/events/create_or_edit.html", form=form)
 
-    event = models.Event()
     form.populate_obj(event)
 
-    if event_id:
-        event = models.Event.objects(event=event)
-        form = forms.events.EventForm(object=event)
+    if not event_id:
+        event.created_by = current_user._get_current_object()
 
-    event.created_by = current_user._get_current_object()
+    event.updated_date = datetime.datetime.now()
     event.updated_by = current_user._get_current_object()
     event.save()
+
     return redirect(url_for("admin.events.index"))
