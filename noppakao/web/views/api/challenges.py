ea0c4cb59f018e63aeeaaad097b48c1795e1511f
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2024-07-20 22:22:30 +0700

    ✨ feat:show detail challenge

---
+++ noppakao/web/views/api/challenges.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, request, send_file, url_for, abort
+from flask.json import jsonify
+from flask_login import current_user, login_required
+from mongoengine import Q
+
+from noppakao import models
+from noppakao.web import acl
+
+module = Blueprint("challenge", __name__, url_prefix="/challenge")
+
+
+@module.route("/<challenge_id>")
+def get_all_data(challenge_id):
+    challenge = models.Challenge.objects(id=challenge_id).first()
+    data = {
+        "name": challenge.name,
+        "category": challenge.category.name,
+        "answer_type": challenge.answer_type,
+        "answer": challenge.answer,
+        "description": challenge.description,
+    }
+    return jsonify(data)
