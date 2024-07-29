0ff0b7fd6b16f3e880066e42676ad497270866fb
TxChar <pontogreen1@gmail.com>
2024-07-29 12:43:12 +0700

    feat: 🎸 Aggregate transaction score

--- noppakao/web/views/dashboards.py
+++ noppakao/web/views/dashboards.py
@@ -1,5 +1,6 @@
 import datetime
 import mongoengine as me
+from bson import ObjectId
 
 from flask import (
     Blueprint,
@@ -19,16 +20,191 @@ from .. import oauth
 module = Blueprint("dashboards", __name__, url_prefix="/dashboard")
 
 
-@module.route("/", methods=["GET", "POST"])
+# @module.route("/", methods=["GET", "POST"])
+# @login_required
+# def index():
+#     teams = models.Team.objects(status="active").order_by("-score", "updated_date")
+#     users = models.User.objects(status="active", roles__ne="admin").order_by(
+#         "-score", "updated_date"
+#     )
+
+#     return render_template(
+#         "dashboards/index.html",
+#         teams=teams,
+#         users=users,
+#     )
+
+
+@module.route("/<event_id>/", methods=["GET", "POST"])
 @login_required
-def index():
-    teams = models.Team.objects(status="active").order_by("-score", "updated_date")
-    users = models.User.objects(status="active", roles__ne="admin").order_by(
-        "-score", "updated_date"
-    )
+def index(event_id):
+    challenges = models.Challenge.objects()
+    teams = models.Team.objects(status="active")
+    users = models.User.objects(status="active")
+
+    event = models.Event.objects(id=event_id).first()
+    event_challenges = models.EventChallenge.objects(event=event, status="active")
+    event_categorys = []
+    dialog_state = {"status": request.args.get("dialog_state", None)}
+    team = models.Team.objects(members__in=[current_user], status="active").first()
+
+    if not team:
+        msg = "Please create a team."
+        return redirect(url_for("events.index", msg=msg))
+
+    if not current_user.check_join_event(event.id):
+        msg = "คุณกำลังพยายามเข้าไปใน กิจกรรมที่คุณไม่ได้เข้าร่วม"
+        return redirect(url_for("events.index", msg=msg))
+
+    for event_challenge in event_challenges:
+        if not event_challenge.challenge.category in event_categorys:
+            event_categorys.append(event_challenge.challenge.category)
+
+    if event.type == "team":
+
+        pipeline_user = [
+            {"$match": {"event": ObjectId(event_id)}},
+            {
+                "$group": {
+                    "_id": {
+                        "user": "$user",
+                        "team": "$team",
+                    },
+                    "total_score": {"$sum": "$score"},
+                }
+            },
+            {
+                "$lookup": {
+                    "from": "users",
+                    "localField": "_id.user",
+                    "foreignField": "_id",
+                    "as": "user_info",
+                },
+            },
+            {"$unwind": "$user_info"},
+            {
+                "$lookup": {
+                    "from": "teams",
+                    "localField": "_id.team",
+                    "foreignField": "_id",
+                    "as": "team_info",
+                }
+            },
+            {"$unwind": "$team_info"},
+            {
+                "$project": {
+                    "_id": 0,
+                    "event": "$_id.event",
+                    "total_score": 1,
+                    "display_name": "$user_info.display_name",
+                    "team": "$team_info.name",
+                    "picture": "$team_info.picture",
+                }
+            },
+            {"$sort": {"total_score": -1}},
+        ]
+
+        pipeline_team = [
+            {"$match": {"event": ObjectId(event_id)}},
+            {
+                "$group": {
+                    "_id": {
+                        "team": "$team",
+                        "event": ObjectId(event_id),
+                    },
+                    "total_score": {"$sum": "$score"},
+                }
+            },
+            {
+                "$lookup": {
+                    "from": "teams",
+                    "localField": "_id.team",
+                    "foreignField": "_id",
+                    "as": "team_info",
+                },
+            },
+            {"$unwind": "$team_info"},
+            {
+                "$project": {
+                    "_id": 0,
+                    "total_score": 1,
+                    "event": "$_id.event",
+                    "name": "$team_info.name",
+                    "picture": "$team_info.picture",
+                }
+            },
+            {"$sort": {"total_score": -1}},
+        ]
+
+        teams_transaction = list(models.Transaction.objects.aggregate(pipeline_team))
+        users_transaction = list(models.Transaction.objects.aggregate(pipeline_user))
+
+        return render_template(
+            "/dashboards/index.html",
+            event_challenges=event_challenges,
+            event=event,
+            event_categorys=event_categorys,
+            teams=teams,
+            users=users,
+            challenges=challenges,
+            dialog_state=dialog_state,
+            teams_transaction=teams_transaction,
+            users_transaction=users_transaction,
+        )
+
+    else:
+        pipeline_user = [
+            {"$match": {"event": ObjectId(event_id)}},
+            {
+                "$group": {
+                    "_id": {
+                        "user": "$user",
+                        "team": "$team",
+                    },
+                    "total_score": {"$sum": "$score"},
+                }
+            },
+            {
+                "$lookup": {
+                    "from": "users",
+                    "localField": "_id.user",
+                    "foreignField": "_id",
+                    "as": "user_info",
+                },
+            },
+            {"$unwind": "$user_info"},
+            {
+                "$lookup": {
+                    "from": "teams",
+                    "localField": "_id.team",
+                    "foreignField": "_id",
+                    "as": "team_info",
+                }
+            },
+            {"$unwind": "$team_info"},
+            {
+                "$project": {
+                    "_id": 0,
+                    "event": "$_id.event",
+                    "total_score": 1,
+                    "display_name": "$user_info.display_name",
+                    "team": "$team_info.name",
+                    "picture": "$team_info.picture",
+                }
+            },
+            {"$sort": {"total_score": -1}},
+        ]
+
+        users_transaction = list(models.Transaction.objects.aggregate(pipeline_user))
 
     return render_template(
-        "dashboards/index.html",
+        "/dashboards/index.html",
+        event_challenges=event_challenges,
+        event=event,
+        event_categorys=event_categorys,
         teams=teams,
         users=users,
+        challenges=challenges,
+        dialog_state=dialog_state,
+        users_transaction=users_transaction,
     )
