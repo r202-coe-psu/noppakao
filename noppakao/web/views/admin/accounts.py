b894702457d09ef9af95e2354a7d75b8b2372182
นพเก้า ปัญจสุธารส <myosuka@gmail.com>
2024-08-07 20:58:36 +0700

    ✨ feat:edit user

--- noppakao/web/views/admin/accounts.py
+++ noppakao/web/views/admin/accounts.py
@@ -130,3 +130,51 @@ def create_or_edit(user_id):
     return redirect(
         url_for("admin.accounts.index"),
     )
+
+
+@module.route("/<user_id>/edit_user", methods=["GET", "POST"])
+@acl.roles_required("admin")
+def edit_user(user_id):
+    form = forms.accounts.RegistrationForm()
+    user = models.User.objects()
+    msg_error = ""
+    user = models.User.objects.get(id=user_id)
+    form = forms.accounts.UpdateUserForm(obj=user)
+    form.username.validators = []
+    form.password.validators = []
+
+    if not form.validate_on_submit():
+        user.username = form.username.data
+
+        return render_template(
+            "/admin/accounts/edit_user.html",
+            form=form,
+            user=user,
+            msg_error=msg_error,
+        )
+
+    check_user = models.User.objects(username=form.username.data)
+    check_email = models.User.objects(email=form.email.data)
+
+    if check_user and not "edit" in request.path:
+        msg_error = "This user is already in use"
+        return render_template(
+            "/admin/accounts/create_edit.html",
+            form=form,
+            user=user,
+            msg_error=msg_error,
+        )
+    if check_email and not "edit" in request.path:
+        msg_error = "This email is already in use"
+        return render_template(
+            "/admin/accounts/create_edit.html",
+            form=form,
+            user=user,
+            msg_error=msg_error,
+        )
+
+    form.populate_obj(user)
+    user.save()
+    return redirect(
+        url_for("admin.accounts.index"),
+    )
