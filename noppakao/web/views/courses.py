955d0b5ba9ffc9dbbac8a5c3a6e4ba30e2808288
Soravit Sukkarn <soravit.sukkarn@gmail.com>
2025-06-15 21:32:44 +0700

    add: course route

---
+++ noppakao/web/views/courses.py
@@ -0,0 +1,42 @@
+from flask import (
+    Blueprint,
+    render_template,
+    url_for,
+    request,
+    session,
+    redirect,
+)
+from flask_login import login_user, logout_user, login_required, current_user
+
+
+module = Blueprint("course", __name__, url_prefix="/course")
+
+# TODO: Add authentication 
+
+@module.route("/", methods=["GET"])
+def index():
+    return render_template(
+        "courses/index.html",
+    )
+
+@module.route("/<course_id>", methods=["GET"])
+def course_detail(course_id):
+    return render_template(
+        "courses/course_detail.html",
+        course_id=course_id,
+    )
+
+@module.route("/<course_id>/content/<page_id>", methods=["GET"])
+def course_content(course_id, page_id):
+    return render_template(
+        "courses/content.html",
+        course_id=course_id,
+        page_id=page_id,
+    )
+
+@module.route("/dashboard", methods=["GET"])
+def dashboard():
+    return render_template(
+        "courses/dashboard.html",
+        courses=[],
+    )
