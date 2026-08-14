b24a85a8fc797fd83e58dd40e69270293172713d
Soravit <soravit.sukkarn@gmail.com>
2026-08-14 20:55:33 +0700

    feat: add interactive admin password prompt to init-admin script

--- scripts/init-admin.py
+++ scripts/init-admin.py
@@ -1,4 +1,5 @@
 import sys
+import getpass
 import mongoengine as me
 from noppakao import models
 import datetime
@@ -7,18 +8,31 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt()
 
 
-def check_has_user_admin_and_reset_pwd():
+def get_admin_password():
+    while True:
+        password = getpass.getpass("Enter admin password: ")
+        if not password:
+            print("Password cannot be empty. Please try again.")
+            continue
+        confirm_password = getpass.getpass("Confirm admin password: ")
+        if password != confirm_password:
+            print("Passwords do not match. Please try again.")
+            continue
+        return password
+
+
+def check_has_user_admin_and_reset_pwd(password):
     print("Checking has user admin")
     user = models.User.objects(username="admin").first()
     if user:
-        user.set_password("p@ssw0rd")
+        user.set_password(password)
         user.save()
         print("There is a user admin.\nReset admin password: Done")
         return True
     return False
 
 
-def create_user_admin():
+def create_user_admin(password):
     print("start create admin")
     user = models.User(
         username="admin",
@@ -29,7 +43,7 @@ def create_user_admin():
         email="admin@example.com",
         roles=["user", "admin"],
     )
-    user.set_password("p@ssw0rd")
+    user.set_password(password)
 
     user.save()
     print("finish")
@@ -40,8 +54,11 @@ if __name__ == "__main__":
         me.connect(db="noppakaodb", host=sys.argv[1])
     else:
         me.connect(db="noppakaodb")
+
+    password = get_admin_password()
     print("start create")
-    if not check_has_user_admin_and_reset_pwd():
-        create_user_admin()
+    if not check_has_user_admin_and_reset_pwd(password):
+        create_user_admin(password)
 
     print("end create")
+
